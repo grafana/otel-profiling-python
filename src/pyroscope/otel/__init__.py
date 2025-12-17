a71fd275416e5c5b548ebff466cb8b56a52446f1
Ali <bincyber@users.noreply.github.com>
2025-12-17 08:00:02 +0400

    Tag profiles with span_name label

--- src/pyroscope/otel/__init__.py
+++ src/pyroscope/otel/__init__.py
@@ -12,6 +12,7 @@ import pyroscope
 
 PROFILE_ID_SPAN_ATTRIBUTE_KEY = 'pyroscope.profile.id'
 PROFILE_ID_PYROSCOPE_TAG_KEY = 'span_id'
+SPAN_NAME_PYROSCOPE_TAG_KEY = 'span_name'
 
 def _is_root_span(span: Span):
     return span.parent is None or span.parent.is_remote
@@ -28,10 +29,12 @@ class PyroscopeSpanProcessor(SpanProcessor):
         if _is_root_span(span):
             span.set_attribute(PROFILE_ID_SPAN_ATTRIBUTE_KEY, format(span.context.span_id, "016x"))
             pyroscope.add_thread_tag(threading.get_ident(), PROFILE_ID_PYROSCOPE_TAG_KEY, _get_span_id(span))
+            pyroscope.add_thread_tag(threading.get_ident(), SPAN_NAME_PYROSCOPE_TAG_KEY, span.name)
 
     def on_end(self, span: ReadableSpan) -> None:
         if _is_root_span(span):
             pyroscope.remove_thread_tag(threading.get_ident(), PROFILE_ID_PYROSCOPE_TAG_KEY, _get_span_id(span))
+            pyroscope.remove_thread_tag(threading.get_ident(), SPAN_NAME_PYROSCOPE_TAG_KEY, span.name)
 
     def shutdown(self) -> None:
         pass
