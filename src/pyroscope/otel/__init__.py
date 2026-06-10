392ac93963e3a10e21a9b96b000cac5511cbf33c
Marc Sanmiquel <marcsanmiquel@gmail.com>
2026-06-10 12:34:20 +0200

    feat: add trace_id pprof label (#45)
    * feat: add trace_id pprof label

--- src/pyroscope/otel/__init__.py
+++ src/pyroscope/otel/__init__.py
@@ -12,12 +12,16 @@ import pyroscope
 PROFILE_ID_SPAN_ATTRIBUTE_KEY = 'pyroscope.profile.id'
 PROFILE_ID_PYROSCOPE_TAG_KEY = 'span_id'
 SPAN_NAME_PYROSCOPE_TAG_KEY = 'span_name'
+TRACE_ID_PYROSCOPE_TAG_KEY = 'trace_id'
 
 def _is_root_span(span: Span):
     return span.parent is None or span.parent.is_remote
 
 def _get_span_id(span: Span):
     return format(span.context.span_id, "016x")
+
+def _get_trace_id(span: Span):
+    return format(span.context.trace_id, "032x")
 
 # A span processor that sets a common identifier in spans and profiling samples, so that they can be linked together.
 class PyroscopeSpanProcessor(SpanProcessor):
@@ -26,14 +30,16 @@ class PyroscopeSpanProcessor(SpanProcessor):
         self, span: Span, parent_context: typing.Optional[Context] = None
     ) -> None:
         if _is_root_span(span):
-            span.set_attribute(PROFILE_ID_SPAN_ATTRIBUTE_KEY, format(span.context.span_id, "016x"))
+            span.set_attribute(PROFILE_ID_SPAN_ATTRIBUTE_KEY, _get_span_id(span))
             pyroscope.add_thread_tag(PROFILE_ID_PYROSCOPE_TAG_KEY, _get_span_id(span))
             pyroscope.add_thread_tag(SPAN_NAME_PYROSCOPE_TAG_KEY, span.name)
+            pyroscope.add_thread_tag(TRACE_ID_PYROSCOPE_TAG_KEY, _get_trace_id(span))
 
     def on_end(self, span: ReadableSpan) -> None:
         if _is_root_span(span):
             pyroscope.remove_thread_tag(PROFILE_ID_PYROSCOPE_TAG_KEY, _get_span_id(span))
             pyroscope.remove_thread_tag(SPAN_NAME_PYROSCOPE_TAG_KEY, span.name)
+            pyroscope.remove_thread_tag(TRACE_ID_PYROSCOPE_TAG_KEY, _get_trace_id(span))
 
     def shutdown(self) -> None:
         pass
